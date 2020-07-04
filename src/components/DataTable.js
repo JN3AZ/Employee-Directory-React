a9fd30d59c0e72bcfb3179f6a7cd594f602f3c50
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-07-04 00:28:20 -0700

    created a dataBody.js and set up logic to create the date and return the tablebody also added a datBody.css to set up later

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,40 +4,39 @@ import "../styles/DataTable.css";
 import ThemeContext from "../utilities/ThemeContext";
 
 const DataTable = () => {
-    const data = useData(ThemeContext);
-  
-    return (
-  
-      <div className="datatable mt-5">
-        <table
-          id="table"
-          className="table table-striped table-hover table-condensed"
-        >
-          <thead>
-            <tr>
-              {context.developerState.headings.map(({ name, width }) => {
-                return (
-                  <th
-                    className="col"
-                    key={name}
-                    style={{ width }}
-                    onClick={() => {
-                      // context.handleOrder(name.toLowerCase());
-                      context.handleOrder(name);
-                    }}
-                  >
-                    {name}
-                    <span className="pointer"></span>
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-  
-          <DataBody />
-        </table>
-      </div>
-    );
-  }
-  
-  export default DataTable;
+  const data = useData(ThemeContext);
+
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+            {context.developerState.headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    // context.handleOrder(name.toLowerCase());
+                    context.handleOrder(name);
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+        <DataBody />
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
